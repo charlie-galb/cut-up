fa6d0082e96be5306c7e68fc69236f33e4949d96
charlie-galb <galbraith.charlie@gmail.com>
2021-05-04 15:26:25 +0100

    Replace hardcoded message with App

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Hello world</h1>, document.querySelector('#root'))
+import App from './App'
+
+ReactDOM.render(<App/>, document.querySelector('#root'))
