fc0b6650e66eaea4af3462dc830e91621fecc824
charlie-galb <galbraith.charlie@gmail.com>
2021-05-05 11:52:17 +0100

    Add presets

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
         test: /\.(js|jsx)/,
         exclude: /node_modules/,
         use: ["babel-loader"],
+        options: { presets: ['@babel/preset-react', '@babel/preset-env'] }
       },
       {
         test: /\.scss/,
