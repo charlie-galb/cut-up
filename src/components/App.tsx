2e370c6659f9ee716edf4168a7f2aed76f1eedba
charlie-galb <galbraith.charlie@gmail.com>
2021-05-07 14:22:23 +0100

    Add snipText function and import useState

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react"
+
+import { CuttingBoard } from "./CuttingBoard/CuttingBoard"
 
 export const App = () => {
+
+  const [snippedText, setSnippedText] = useState<string[]>()
+
+  const snipText = (text: string) => {
+    const temp = text.split(" ")
+    const result: Array<string> = []
+    for(let i = 0; i < temp.length; i = i + 2 ) {
+      result.push(temp.slice(i,i+2).join('.'))
+    }
+    setSnippedText(result)
+  }
+
   return (
-    <h1 data-testid="test-header">
-      Cut-up App
-    </h1>
+    <div>
+      <h1 data-testid="test-header">Cut-up App</h1>
+      <CuttingBoard snipText={snipText}/>
+    </div>
+    
   )
 }
