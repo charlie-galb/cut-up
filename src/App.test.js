2407f5c5b8e34120efca4fdff919300c3208c9ff
charlie-galb <galbraith.charlie@gmail.com>
2021-05-07 11:19:29 +0100

    Convert to typescript

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from "@testing-library/react";
-
-import App from './App';
-
-test('renders without crashing', () => {
-    const { getByTestId } = render(<App />);
-    expect(getByTestId("test-header")).toHaveTextContent("Cut-up App");
-});
