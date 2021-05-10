33c3ba3cff0e1c8dbe0b6a3a0470f418814b53f3
charlie-galb <galbraith.charlie@gmail.com>
2021-05-10 12:23:55 +0100

    Add draggable wrapper

--- src/components/TextSnippet/TextSnippet.tsx
+++ src/components/TextSnippet/TextSnippet.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
+import { Draggable } from "react-beautiful-dnd"
+
 interface Props {
     text: string
+    index: number
 }
 
 export const TextSnippet = (props: Props) => {
     return (
-        <div role="snippet">{props.text}</div>
+        <Draggable draggableId={`snippet${props.index}`} index={props.index}>
+            {provided => (
+                <div 
+                className="text-snippet"
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                >
+                    {props.text}
+                </div>
+            )}
+        </Draggable>
     )
 }
