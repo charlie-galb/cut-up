6da73b1cca4544a7ebb174028ca9348a7aab4eef
charlie-galb <galbraith.charlie@gmail.com>
2021-05-10 10:17:54 +0100

    Reformat filename to avoid confusion with jest files

--- cypress/integration/visit_page.spec.js
+++
@@ -1,6 +0,0 @@
-describe("Cut-up app", () => {
-    it('Has a main page with a title', () => {
-        cy.visit('http://localhost:3000/')
-        cy.contains("Cut-up App").should("be.visible")
-    })
-})
