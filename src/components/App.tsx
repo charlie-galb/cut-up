110a4ef59f171ef916083468ab27e1fb8e76dc7f
charlie-galb <galbraith.charlie@gmail.com>
2021-05-10 15:30:00 +0100

    Add work in progress component and update onDragEnd

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,33 +3,61 @@ import { DragDropContext, DropResult } from 'react-beautiful-dnd'
 
 import { CuttingBoard } from "./CuttingBoard/CuttingBoard"
 import { PasteBoard } from "./PasteBoard/PasteBoard"
+import { WorkInProgress } from "./WorkInProgress/WorkInProgress"
+import { chunk } from "../types/chunk"
 
 export const App = () => {
 
-  const [snippedText, setSnippedText] = useState<string[]>([""])
+  const [chosenText, setChosenText] = useState<chunk[]>([])
+  const [snippedText, setSnippedText] = useState<chunk[]>([])
 
   const snipText = (text: string) => {
     const temp = text.split(" ")
-    const result: Array<string> = []
+    const result: Array<chunk> = []
+    let id_acc = 1
     for(let i = 0; i < temp.length; i = i + 2 ) {
-      result.push(temp.slice(i,i+2).join(' '))
+      const chunk: chunk = { id: id_acc, text: temp.slice(i,i+2).join(' ')}
+      result.push(chunk)
+      id_acc++
     }
     setSnippedText(result)
   }
 
   const onDragEnd = (result: DropResult) => {
-    if (!result.destination) return
-    if (
+    if (!result.destination) {
+      return
+    } else if (
       result.source.droppableId === result.destination.droppableId &&
       result.source.index === result.destination.index
     ) {
       return; 
+    } else if (
+      result.source.droppableId === result.destination.droppableId && 
+      result.destination.droppableId === "snippet-bank"
+    ) {
+      const newChunks = snippedText;
+      const draggedItem = newChunks[result.source.index]
+      newChunks.splice(result.source.index, 1);
+      newChunks.splice(result.destination.index, 0, draggedItem);
+      setSnippedText(newChunks);
+    } else if (
+      result.source.droppableId === result.destination.droppableId && 
+      result.destination.droppableId === "wip"
+    ) {
+      const newChosen = chosenText;
+      const draggedItem = newChosen[result.source.index]
+      newChosen.splice(result.source.index, 1);
+      newChosen.splice(result.destination.index, 0, draggedItem);
+      setChosenText(newChosen);
+    } else {
+      const newChunks = snippedText;
+      const updatedPoem = chosenText
+      const draggedItem = newChunks[result.source.index]
+      newChunks.splice(result.source.index, 1);
+      updatedPoem.splice(result.destination.index, 0, draggedItem);
+      setSnippedText(newChunks);
+      setChosenText(updatedPoem);
     }
-    const newChunks = snippedText;
-    const draggedItem = newChunks[result.source.index]
-    newChunks.splice(result.source.index, 1);
-    newChunks.splice(result.destination.index, 0, draggedItem);
-    setSnippedText(newChunks);
   }
 
   return (
@@ -38,6 +66,7 @@ export const App = () => {
       <CuttingBoard snipText={snipText}/>
       <DragDropContext onDragEnd={onDragEnd}>
         <PasteBoard wordChunks={snippedText} />
+        <WorkInProgress poemChunks={chosenText} />
       </DragDropContext>
     </div>
     
