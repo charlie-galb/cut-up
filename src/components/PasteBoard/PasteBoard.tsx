8e7214d28e06609fff1e9763bbac2c2f9d48eaf8
charlie-galb <galbraith.charlie@gmail.com>
2021-05-10 11:26:27 +0100

    Add droppable wrapper and custom TextSnippet component

--- src/components/PasteBoard/PasteBoard.tsx
+++ src/components/PasteBoard/PasteBoard.tsx
@@ -1,5 +1,9 @@
 import React, {useEffect } from "react"
 
+import { Droppable } from "react-beautiful-dnd"
+
+import { TextSnippet } from "../TextSnippet/TextSnippet"
+
 interface Props {
     wordChunks: string[]
 }
@@ -10,13 +14,20 @@ export const PasteBoard = (props: Props) => {
 
     return (
         <div className="pasteboard-container" >
-            <ul>
-                {wordChunks.map((chunk, i) => {
-                    return (
-                        <li key={i}>{chunk}</li>
-                    )
-                })}
-            </ul>
+            <Droppable droppableId="1">
+                {provided => (
+                <div data-testid="unused-snippets" {...provided.droppableProps}
+                    ref={provided.innerRef}>
+                    {wordChunks.map((chunk, i) => {
+                        return (
+                            <TextSnippet data-testid="snippet" key={i} text={chunk}/>
+                        )
+                    })}
+                    {provided.placeholder}
+                </div>
+
+                )}
+            </Droppable>
         </div>
     )
 }
