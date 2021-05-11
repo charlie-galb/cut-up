a6da7b89676e70ec973d0edbb7b8b5abb105ab7f
charlie-galb <galbraith.charlie@gmail.com>
2021-05-11 11:29:21 +0100

    Add test for inter-list drag and drop

--- cypress/integration/drag_and_drop_spec.js
+++ cypress/integration/drag_and_drop_spec.js
@@ -1,18 +1,23 @@
 describe("drag and drop", () => {
-    it("user can drag and drop text chunks from the pasteboard into their current poem", () => {
+    it("user can reorder chunks in the pasteboard using drag and drop", () => {
         cy.visit('http://localhost:3000/')
         cy.get('[data-testid="cutting-text-area"]').type("one and two and")
         cy.get('[data-testid="cut-btn"]').click()
-        cy.get('.text-snippet').first().should("have.text", "one and")
-        cy.get('.text-snippet').first().next().should("have.text", "two and")
         cy.get(".text-snippet").contains("one and")
             .focus()
             .trigger('keydown', { keyCode: 32 })
-            .trigger('keydown', { keyCode: 40, force: true })
+            .trigger('keydown', { keyCode: 39, force: true })
             .wait(500)
             .trigger('keydown', { keyCode: 32, force: true })
             .trigger('keydown', { keyCode: 32, force: true });
         cy.get('.text-snippet').first().should("have.text", "two and")
         cy.get('.text-snippet').first().next().should("have.text", "one and")
+    })
+    it("user can move chunks from the pasteboard into their current work-in-progress using drag and drop", () => {
+        cy.visit('http://localhost:3000/')
+        cy.get('[data-testid="cutting-text-area"]').type("one and two and")
+        cy.get('[data-testid="cut-btn"]').click()
+        cy.dragAndDrop(".text-snippet", ".wip-container")
+        cy.get('[data-testid="wip-snippets"] .text-snippet').first().should("have.text", "one and")
     })
 })
