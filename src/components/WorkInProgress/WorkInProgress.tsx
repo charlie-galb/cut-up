0721f70e2d633db928e778bda032dbcf51de6df0
charlie-galb <galbraith.charlie@gmail.com>
2021-05-11 16:04:30 +0100

    Update args to usechunkContainer not chunkArrays

--- src/components/WorkInProgress/WorkInProgress.tsx
+++ src/components/WorkInProgress/WorkInProgress.tsx
@@ -3,25 +3,26 @@ import React from 'react'
 import { Droppable } from "react-beautiful-dnd"
 
 import { TextSnippet } from "../TextSnippet/TextSnippet"
-import { chunk } from "../../types/chunk"
+import { chunkContainer } from "../../types/chunkContainer"
 
 interface Props {
-    poemChunks: chunk[]
+    chunkContainer: chunkContainer
 }
 
 export const WorkInProgress = (props: Props) => {
 
-    const { poemChunks } = props
+    const { chunkContainer } = props
+    const { id, nestedChunks } = chunkContainer
 
     return (
         <div className="wip-container">
-            <Droppable droppableId="wip" direction="horizontal">
+            <Droppable droppableId={id} direction="horizontal">
                 {provided => (
                 <div data-testid="wip-snippets" 
                 className="wip-snippets"
                 {...provided.droppableProps}
                 ref={provided.innerRef}>
-                    {poemChunks?.map((chunk, i) => {
+                    {nestedChunks?.map((chunk, i) => {
                         return (
                             <TextSnippet data-testid="snippet" key={chunk.id} id={chunk.id} index={i} text={chunk.text}/>
                         )
