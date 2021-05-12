3684e55c4d1825de91a8fa22954b39569d0945f4
charlie-galb <galbraith.charlie@gmail.com>
2021-05-12 11:32:44 +0100

    Add test for dynamic rendering of lines

---
+++ src/components/WorkInProgress/WorkInProgress.test.tsx
@@ -0,0 +1,49 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import { DragDropContext } from 'react-beautiful-dnd'
+ 
+import { WorkInProgress } from "./WorkInProgress"
+import { chunk } from "../../types/chunk"
+import { chunkContainer } from "../../types/chunkContainer"
+
+const chunkOne: chunk = {
+    id: 1,
+    text: "one"
+}
+
+const chunkTwo: chunk = {
+    id: 2,
+    text: "two"
+}
+
+const wordChunks = [chunkOne, chunkTwo]
+
+const mockContainer1: chunkContainer = {
+    id: 'mockLineOne',
+    title: 'mockLineOne',
+    nestedChunks: wordChunks
+  }
+
+const mockContainer2: chunkContainer = {
+    id: 'mockLineTwo',
+    title: 'mockLineTwo',
+    nestedChunks: wordChunks
+}
+
+const mockContainers = {
+    [mockContainer1.id]: mockContainer1,
+    [mockContainer2.id]: mockContainer2,
+}
+
+const mockLineIds = [mockContainer1.id, mockContainer2.id]
+
+describe("WorkInProgress", () => {
+    it("iterates over the lines array and renders every element", () => {
+        const { getAllByRole } = render(
+        <DragDropContext onDragEnd={() => {}}>
+            <WorkInProgress lineOrder={mockLineIds} chunkContainers={mockContainers} />
+        </DragDropContext>
+        )
+        expect(getAllByRole("button")).toHaveLength(4)
+    })
+})
