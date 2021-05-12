3027c7199f74ca2733f5d39df34022c32edf3b0e
charlie-galb <galbraith.charlie@gmail.com>
2021-05-12 11:33:00 +0100

    Update WIP args with lineOrder and chunkContainers

--- src/components/CraftingBoard/CraftingBoard.tsx
+++ src/components/CraftingBoard/CraftingBoard.tsx
@@ -9,13 +9,14 @@ interface Props {
     chunkContainers: {
       [key: string]: chunkContainer
     }
+    lineOrder: string[]
   }
 
 export const CraftingBoard = (props: Props) => {
     return (
         <div style={{display: "flex", flexDirection: "row", width: "80%", zIndex: -14 }}>
             <PasteBoard chunkContainer={props.chunkContainers['chunk-container-1']} />
-            <WorkInProgress chunkContainer={props.chunkContainers['chunk-container-2']} />
+            <WorkInProgress lineOrder={props.lineOrder} chunkContainers={props.chunkContainers} />
         </div>
     )
 }
