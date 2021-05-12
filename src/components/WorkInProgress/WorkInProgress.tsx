4483e5e0b7661d525cd2697d0c143774a68fdf4f
charlie-galb <galbraith.charlie@gmail.com>
2021-05-12 10:54:14 +0100

    Extract droppable logic to line component

--- src/components/WorkInProgress/WorkInProgress.tsx
+++ src/components/WorkInProgress/WorkInProgress.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Droppable } from "react-beautiful-dnd"
 
 import { TextSnippet } from "../TextSnippet/TextSnippet"
+import { Line } from "../Line/Line"
 import { chunkContainer } from "../../types/chunkContainer"
 
 interface Props {
@@ -12,25 +13,10 @@ interface Props {
 export const WorkInProgress = (props: Props) => {
 
     const { chunkContainer } = props
-    const { id, nestedChunks } = chunkContainer
 
     return (
         <div className="wip-container">
-            <Droppable droppableId={id} direction="horizontal">
-                {provided => (
-                <div data-testid="wip-snippets" 
-                className="wip-snippets"
-                {...provided.droppableProps}
-                ref={provided.innerRef}>
-                    {nestedChunks?.map((chunk, i) => {
-                        return (
-                            <TextSnippet data-testid="snippet" key={chunk.id} id={chunk.id} index={i} text={chunk.text}/>
-                        )
-                    })}
-                    {provided.placeholder}
-                </div>
-                )}
-            </Droppable>
+            <Line chunkContainer={chunkContainer}/>
         </div>
     )
 }
