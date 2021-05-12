13a2aec8fc41ab057bb7245148ff8926ab240e77
charlie-galb <galbraith.charlie@gmail.com>
2021-05-12 12:50:42 +0100

    Handle adding new lines

--- src/components/WorkInProgress/WorkInProgress.tsx
+++ src/components/WorkInProgress/WorkInProgress.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
 
-import { Droppable } from "react-beautiful-dnd"
-
-import { TextSnippet } from "../TextSnippet/TextSnippet"
 import { Line } from "../Line/Line"
 import { chunkContainer } from "../../types/chunkContainer"
 
@@ -11,19 +8,27 @@ interface Props {
         [key: string]: chunkContainer
       }
     lineOrder: string[]
+    addLine: (arg: void) => void
 }
 
 export const WorkInProgress = (props: Props) => {
 
-    const { chunkContainers, lineOrder } = props
+    const { chunkContainers, lineOrder, addLine } = props
+
+    const handleLineAdding = () => {
+        addLine()
+    }
 
     return (
         <div className="wip-container">
-            {lineOrder?.map((lineId, i) => {
-                return (
-                    <Line key={i} chunkContainer={chunkContainers[lineId]}/>
-                )
-            })}
+            <div className="lines-container">
+                {lineOrder?.map((lineId, i) => {
+                    return (
+                        <Line data-testid="line" key={i} chunkContainer={chunkContainers[lineId]}/>
+                    )
+                })}
+            </div>
+            <button data-testid="add-line-btn" onClick={handleLineAdding} >Add line</button>
         </div>
     )
 }
