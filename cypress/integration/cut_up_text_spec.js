5b869d5525bc59abba87464d49fca345dafefcd2
charlie-galb <galbraith.charlie@gmail.com>
2021-05-13 16:12:09 +0100

    Update test name and add third assertion for third element

--- cypress/integration/cut_up_text_spec.js
+++ cypress/integration/cut_up_text_spec.js
@@ -1,9 +1,10 @@
 describe("Cut-up app", () => {
-    it('Has a main page with a title', () => {
+    it('Cuts text into chunks and removes punctuation/capitalisation', () => {
         cy.visit('http://localhost:3000/')
-        cy.get('[data-testid="cutting-text-area"]').type("one and two and")
+        cy.get('[data-testid="cutting-text-area"]').type("This should be unformatted, no?")
         cy.get('[data-testid="cut-btn"]').click()
-        cy.get('.text-snippet').first().should("have.text", "one and")
-        cy.get('.text-snippet').first().next().should("have.text", "two and")
+        cy.get('.text-snippet').first().should("have.text", "this should")
+        cy.get('.text-snippet').first().next().should("have.text", "be unformatted")
+        cy.get('.text-snippet').first().next().next().should("have.text", "no")
     })
 })
