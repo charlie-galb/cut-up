8fcb467f672318278b2dcaa88c8ffcb7bb7052ff
charlie-galb <galbraith.charlie@gmail.com>
2021-05-14 15:06:22 +0100

    Update tests to include additional cutting areas

--- cypress/integration/cut_up_text_spec.js
+++ cypress/integration/cut_up_text_spec.js
@@ -1,10 +1,12 @@
 describe("Cut-up app", () => {
     it('Cuts text into chunks and removes punctuation/capitalisation', () => {
         cy.visit('http://localhost:3000/')
-        cy.get('[data-testid="cutting-text-area"]').type("This should be unformatted, no?")
+        cy.get('[data-testid="cutting-text-area1"]').type("One and?")
+        cy.get('[data-testid="cutting-text-area2"]').type("Two and!")
+        cy.get('[data-testid="cutting-text-area3"]').type("Three and.")
         cy.get('[data-testid="cut-btn"]').click()
-        cy.get('.text-snippet').first().should("have.text", "this should")
-        cy.get('.text-snippet').first().next().should("have.text", "be unformatted")
-        cy.get('.text-snippet').first().next().next().should("have.text", "no")
+        cy.get('.text-snippet').first().should("have.text", "one and")
+        cy.get('.text-snippet').first().next().should("have.text", "two and")
+        cy.get('.text-snippet').first().next().next().should("have.text", "three and")
     })
 })
