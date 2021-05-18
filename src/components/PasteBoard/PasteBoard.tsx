c5cf150d31b0146936656623823ebff3a167ab82
charlie-galb <galbraith.charlie@gmail.com>
2021-05-18 16:07:40 +0100

    Add activeId prop and dragOverlay

--- src/components/PasteBoard/PasteBoard.tsx
+++ src/components/PasteBoard/PasteBoard.tsx
@@ -1,8 +1,10 @@
 import React from "react"
 
+import { DragOverlay } from "@dnd-kit/core"
 import { SortableContext, rectSortingStrategy} from "@dnd-kit/sortable"
 
 import { TextSnippet } from "../TextSnippet/TextSnippet"
+import { SortableTextSnippet } from "../TextSnippet/SortableTextSnippet"
 import { chunkContainer } from "../../types/chunkContainer"
 
 interface Props {
@@ -10,22 +12,28 @@ interface Props {
     wordChunks: {
         [key: string]: string
       }
+    activeId: string
 }
 
 export const PasteBoard = (props: Props) => {
    
-    const { chunkContainer, wordChunks } = props
+    const { chunkContainer, wordChunks, activeId } = props
     const { id, nestedChunkIDs } = chunkContainer
 
     return (
-        <SortableContext id={id} items={nestedChunkIDs} strategy={rectSortingStrategy}>
-            <div className="unused-snippets" data-testid="unused-snippets" >
-                {nestedChunkIDs?.map((ID, i) => {
-                    return (
-                        <TextSnippet data-testid="snippet" key={ID} id={ID} index={i} text={wordChunks[ID]}/>
-                    )
-                })}
-            </div>
-        </SortableContext>
+        <div> 
+            <SortableContext id={id} items={nestedChunkIDs} strategy={rectSortingStrategy}>
+                <div className="unused-snippets" data-testid="unused-snippets" >
+                    {nestedChunkIDs?.map((ID, i) => {
+                        return (
+                            <SortableTextSnippet data-testid="snippet" key={ID} id={ID} index={i} text={wordChunks[ID]}/>
+                        )
+                    })}
+                </div>
+            </SortableContext>
+            <DragOverlay>
+                {activeId ? <TextSnippet /> : null}
+            </DragOverlay>
+        </div>
     )
 }
