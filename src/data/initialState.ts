9375d4d8d3480dae3f634850e0aab696e91ac1d2
charlie-galb <galbraith.charlie@gmail.com>
2021-05-20 16:11:39 +0100

    deconstruct state object

--- src/data/initialState.ts
+++ src/data/initialState.ts
@@ -1,39 +1,21 @@
 import { chunk } from '../types/chunk'
 import { chunkContainer } from '../types/chunkContainer'
 
-interface initialState {
-    activeId: string
-    overId: string
-    wordChunks: {
-      [key: string]: string
-    }
-    chunkContainers: {
-      [key: string]: chunkContainer
-    }
-    lineOrder: string[],
-    poemAsText: string
-  }
-
-  const pasteBoard: chunkContainer = {
-    id: 'chunk-container-1',
-    title: 'pasteboard',
-    nestedChunkIDs: []
-  }
-
-  const line1: chunkContainer = {
-    id: 'chunk-container-2',
-    title: 'line-1',
-    nestedChunkIDs: []
-  }
-
-export const initialState: initialState = {
-    activeId: "",
-    overId: "",
-    wordChunks: {},
-    chunkContainers: {
-      'chunk-container-1': pasteBoard,
-      'chunk-container-2': line1
-    },
-    lineOrder: ['chunk-container-2'],
-    poemAsText: ""
+const pasteBoard: chunkContainer = {
+  id: 'chunk-container-1',
+  title: 'pasteboard',
+  nestedChunkIDs: []
 }
+
+const line1: chunkContainer = {
+  id: 'chunk-container-2',
+  title: 'line-1',
+  nestedChunkIDs: []
+}
+
+export const initialChunkContainers = {
+  'chunk-container-1': pasteBoard,
+  'chunk-container-2': line1
+}
+
+export const initialLineOrder = ['chunk-container-2']
