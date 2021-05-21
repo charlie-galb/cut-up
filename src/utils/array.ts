802039dd66d39811cdeb44d83b55370fec05f000
charlie-galb <galbraith.charlie@gmail.com>
2021-05-21 17:07:22 +0100

    Add shuffle

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -11,3 +11,14 @@ export const insertAtIndex = (array: string[], index: number, item: string) => {
 export const arrayMove = (array: string[], oldIndex: number, newIndex: number) => {
   return dndKitArrayMove(array, oldIndex, newIndex);
 };
+
+export const shuffle = (array: string[]) => {
+  var m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m]
+    array[m] = array[i]
+    array[i] = t
+  }
+  return array
+}
