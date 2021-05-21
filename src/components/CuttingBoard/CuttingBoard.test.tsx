34ee6af23943ede5677df7654706276e3082f146
charlie-galb <galbraith.charlie@gmail.com>
2021-05-21 14:51:44 +0100

    Add combine text function

--- src/components/CuttingBoard/CuttingBoard.test.tsx
+++ src/components/CuttingBoard/CuttingBoard.test.tsx
@@ -3,23 +3,43 @@ import { render, fireEvent } from "@testing-library/react"
 
 import { CuttingBoard } from "./CuttingBoard"
 
-const snipMock = jest.fn()
+import { 
+    mockContainers, 
+} from "../../utils/mockData"
+
+const mockSetWordChunks = jest.fn()
+const mockSetChunkContainers = jest.fn()
 
 describe("CuttingBoard", () => {
     test("it renders correctly", () => {
-        const board = render(<CuttingBoard snipText ={snipMock}/>)
-        expect(board).toMatchSnapshot()
+        const board = render(
+            <CuttingBoard 
+                chunkContainers={mockContainers}
+                setWordChunks={mockSetWordChunks}
+                setChunkContainers={mockSetChunkContainers}/>
+            )
     })
     test("there are three text areas", () => {
-        const { getAllByRole } = render(<CuttingBoard snipText ={snipMock}/>)
+        const { getAllByRole } = render(
+            <CuttingBoard 
+                chunkContainers={mockContainers}
+                setWordChunks={mockSetWordChunks}
+                setChunkContainers={mockSetChunkContainers}/>
+            )
         expect(getAllByRole("textbox")).toHaveLength(3)
     })
-    test("It combines the three texts and calls snipText", () => {
-        const { getByTestId } = render(<CuttingBoard snipText ={snipMock} />)
+    test("It combines the three texts and saves state", () => {
+        const { getByTestId } = render(
+            <CuttingBoard 
+                chunkContainers={mockContainers}
+                setWordChunks={mockSetWordChunks}
+                setChunkContainers={mockSetChunkContainers}/>
+            )
         fireEvent.change(getByTestId("cutting-text-area1"), {target: {value: "One and" } } )
         fireEvent.change(getByTestId("cutting-text-area2"), {target: {value: "Two and" } } )
         fireEvent.change(getByTestId("cutting-text-area3"), {target: {value: "Three and" } } )
         fireEvent.click(getByTestId("cut-btn"))
-        expect(snipMock).toHaveBeenCalledWith("One and Two and Three and")
+        expect(mockSetWordChunks).toHaveBeenCalledWith({"snippet1": "one and", "snippet2": "two and", "snippet3": "three and"})
+        expect(mockSetWordChunks).toHaveBeenCalledTimes(1)
     })
 })
--- src/components/CuttingBoard/CuttingBoard.tsx
+++ src/components/CuttingBoard/CuttingBoard.tsx
@@ -1,15 +1,63 @@
 import React, { useState } from 'react'
 
+import { CutButton } from "../CutButton/CutButton"
+
+import { chunkContainer } from "../../types/chunkContainer"
+
 interface Props {
-    snipText: (arg: string) => void
+    chunkContainers: {[key: string]: chunkContainer}
+    setChunkContainers: (arg: {[key: string]: chunkContainer}) => void
+    setWordChunks: (arg: { [key: string]: string }) => void
 }
 
 export const CuttingBoard = (props: Props) => {
     
-    const { snipText } = props
+    const { setChunkContainers, setWordChunks, chunkContainers } = props
     const [source1Text, setSource1Text] = useState("")
     const [source2Text, setSource2Text] = useState("")
     const [source3Text, setSource3Text] = useState("")
+
+    const combineText = (textArr: string[]) => {
+        textArr = textArr.filter(item => item)
+        let combinedText: string = ""
+        let endPoint = textArr.length
+        for(let i = 0; i < endPoint; i++) {
+            if (textArr[i] && i != endPoint -1) {
+                combinedText += (textArr[i] + " ")
+            } else if (textArr[i]) {
+                combinedText += textArr[i]
+            }
+        }
+        return combinedText
+    }
+
+    const snipText = (text: string) => {
+        const temp = text.split(" ")
+        const newWordChunks: {[key: string]: string} = {}
+        const newPasteBoardIDs: string[] = []
+        let id_acc = 1
+        for(let i = 0; i < temp.length; i = i + 2 ) {
+          const id = `snippet${id_acc}`
+          const text = temp.slice(i,i+2).join(' ')
+          const unformattedText = removePunctuation(text.toLowerCase())
+          newWordChunks[id] = unformattedText
+          newPasteBoardIDs.push(id)
+          id_acc++
+        }
+        const newPasteBoard = chunkContainers['chunk-container-1']
+        newPasteBoard.nestedChunkIDs = newPasteBoardIDs
+        const newChunkContainers = {
+          ...chunkContainers,
+            [newPasteBoard.id]: newPasteBoard
+          }
+        setChunkContainers(newChunkContainers)
+        setWordChunks(newWordChunks)
+      }
+
+    const removePunctuation = (string: string) => {
+    return string.replace(/[^\w\s]|_/g, "")
+            .replace(/\s+/g, " ");
+    }
     
     const handleCutting = () => {
         const textArr = [source1Text, source2Text, source3Text]
@@ -32,20 +80,6 @@ export const CuttingBoard = (props: Props) => {
         setSource3Text(value)
     }
 
-    const combineText = (textArr: string[]) => {
-        textArr = textArr.filter(item => item)
-        let combinedText: string = ""
-        let endPoint = textArr.length
-        for(let i = 0; i < endPoint; i++) {
-            if (textArr[i] && i != endPoint -1) {
-                combinedText += (textArr[i] + " ")
-            } else if (textArr[i]) {
-                combinedText += textArr[i]
-            }
-        }
-        return combinedText
-    }
-
     return (
         <div className="cutting-container">
             <div className='text-area-container'>
@@ -53,7 +87,7 @@ export const CuttingBoard = (props: Props) => {
                 <textarea data-testid="cutting-text-area2" className="cutting-text-area 2" cols={10} rows={4} placeholder="Paste your text sources here" value={source2Text} onChange={handleSource2TextChange}/>
                 <textarea data-testid="cutting-text-area3" className="cutting-text-area 3" cols={10} rows={4} placeholder="Paste your text sources here" value={source3Text} onChange={handleSource3TextChange}/>
             </div>
-            <button data-testid="cut-btn" className="cut-btn" onClick={handleCutting} >Cut up</button>
+            <CutButton handleCallback={handleCutting} />
         </div>
     )
 }
