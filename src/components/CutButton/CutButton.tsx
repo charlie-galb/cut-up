1f4b9e04f2c8593a1020a44f1ddb5fac405f3199
charlie-galb <galbraith.charlie@gmail.com>
2021-05-21 14:52:02 +0100

    Add handleCallback argument

---
+++ src/components/CutButton/CutButton.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface Props {
+    handleCallback: (event: any) => void
+}
+
+export const CutButton = (props: Props) => {
+    const { handleCallback } = props
+    return (
+        <div>
+            <button data-testid="cut-btn" className="cut-btn" onClick={handleCallback} >Cut up</button>
+        </div>
+    )
+}
