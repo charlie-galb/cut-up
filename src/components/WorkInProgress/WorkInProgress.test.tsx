a20145d46b76b003bb579c22f35157eb5a07ee5d
charlie-galb <galbraith.charlie@gmail.com>
2021-05-21 14:51:27 +0100

    Update imports and args

--- src/components/WorkInProgress/WorkInProgress.test.tsx
+++ src/components/WorkInProgress/WorkInProgress.test.tsx
@@ -3,42 +3,24 @@ import { render } from "@testing-library/react"
 import { DndContext } from '@dnd-kit/core'
  
 import { WorkInProgress } from "./WorkInProgress"
-import { chunk } from "../../types/chunk"
-import { chunkContainer } from "../../types/chunkContainer"
+import { mockChunks, 
+    mockLineOrder, 
+    mockContainers} from "../../utils/mockData"
 
-const mockAddLine = jest.fn()
-
-const mockChunks = {
-    "1": "one",
-    "2": "two"
-}
-
-const mockChunkIDs = ["1", "2"]
-
-const mockContainer1: chunkContainer = {
-    id: 'chunk-container-1',
-    title: 'pasteboard',
-    nestedChunkIDs: mockChunkIDs
-  }
-
-  const mockContainer2: chunkContainer = {
-    id: 'chunk-container-2',
-    title: 'Line-1',
-    nestedChunkIDs: mockChunkIDs
-  }
-
-const mockContainers = {
-    [mockContainer1.id]: mockContainer1,
-    [mockContainer2.id]: mockContainer2,
-}
-
-const mockLineIds = [mockContainer1.id, mockContainer2.id]
+const mockSetLineOrder = jest.fn()
+const mockSetChunkContainers = jest.fn()
 
 describe("WorkInProgress", () => {
     it("iterates over the lines array and renders every element", () => {
         const { getAllByRole } = render(
         <DndContext onDragEnd={() => {}}>
-            <WorkInProgress activeId={""} wordChunks={mockChunks} lineOrder={mockLineIds} chunkContainers={mockContainers} addLine={mockAddLine}/>
+            <WorkInProgress 
+                activeId={""} 
+                wordChunks={mockChunks} 
+                lineOrder={mockLineOrder} 
+                chunkContainers={mockContainers} 
+                setLineOrder={mockSetLineOrder} 
+                setChunkContainers={mockSetChunkContainers}/>
         </DndContext>
         )
         expect(getAllByRole("button")).toHaveLength(5)
