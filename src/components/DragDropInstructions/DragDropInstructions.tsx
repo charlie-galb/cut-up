fffe28c599e8fd3423fa88cbfbf870c47d989487
charlie-galb <galbraith.charlie@gmail.com>
2021-05-27 16:12:24 +0100

    Add text

---
+++ src/components/DragDropInstructions/DragDropInstructions.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const DragDropInstructions = () => {
+    return (
+        <div className="drag-drop-instructions">
+            <p>Drag and drop the snippets into the blank lines provided,
+                creating whatever weird combinations you like. </p><br />
+            <p>When you're happy with what you've made, hit the textify 
+                button. Your poem will appear as text below.</p><br />
+            <p>And just like that, you're a poet. </p>
+        </div>
+    )
+}
