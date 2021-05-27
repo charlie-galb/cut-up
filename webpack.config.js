d22f16b30f9ce9375e98eb1947f878e79f27c218
charlie-galb <galbraith.charlie@gmail.com>
2021-05-27 15:02:05 +0100

    Add file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,11 @@ module.exports = {
       {
         test: /\.scss/,
         use: ['style-loader', 'css-loader', 'sass-loader']
-     }
+     },
+     { 
+      test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+      use: ["file-loader"] 
+    }
     ],
   },
   resolve: {
