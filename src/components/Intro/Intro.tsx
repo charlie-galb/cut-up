f9e9874f97e141e8c13991e306cad39ed3761c0b
charlie-galb <galbraith.charlie@gmail.com>
2021-05-27 16:13:03 +0100

    remove inner container div and extract half text to DragDropInstructions

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -3,17 +3,13 @@ import React from 'react'
 export const Intro = () => {
     return (
         <div className='intro-container'>
-            <p className='intro-text' data-testid="intro-text" >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id mauris id enim imperdiet gravida. Donec 
-                porttitor lectus eu nisi vulputate ornare. Phasellus semper a dolor a fringilla. Curabitur quis nisl vel
-                tortor iaculis cursus eget quis nisl. Sed sodales urna vitae dolor faucibus bibendum. Mauris sit amet 
-                posuere urna, sed auctor dolor. Etiam sodales scelerisque lacus, a ultricies neque blandit et. Vivamus 
-                quis enim at orci tristique pharetra. Aliquam consequat elit in nisi elementum, vitae interdum neque 
-                pretium. Vivamus mauris enim, semper at pharetra sit amet, aliquam laoreet turpis. Pellentesque habitant 
-                morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam lacinia pharetra diam eget 
-                tincidunt. Donec dictum vestibulum arcu mattis volutpat. Nunc id eros lacus. Sed molestie tristique diam, 
-                et blandit magna tristique id.
-            </p>
+            <p>Cut Up Creator is a tool for making collage poems 
+                in the style of William S. Burroughs.</p>
+            <br />
+            <p>Just copy text from three different sources (the more 
+                different the better) and paste them in the 
+                boxes below. Then hit the cut-up button to shred the text.</p>
+            <br />
         </div>
     )
 }
