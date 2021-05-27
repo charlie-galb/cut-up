aaab75c378b9e723e6e2a393d8052c7ab282b159
charlie-galb <galbraith.charlie@gmail.com>
2021-05-27 16:12:35 +0100

    Rename to cut-up creator

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <div className="header">
-            <h1 data-testid="header">Cut-up App</h1>
+            <h1 data-testid="header">Cut-up Creator</h1>
         </div>
     )
 }
