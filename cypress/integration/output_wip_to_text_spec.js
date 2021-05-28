908bd77ce66197bb8dd1b833a1c740133152a85e
charlie-galb <galbraith.charlie@gmail.com>
2021-05-28 15:42:26 +0100

    Update test-id in assertion

--- cypress/integration/output_wip_to_text_spec.js
+++ cypress/integration/output_wip_to_text_spec.js
@@ -1,13 +1,14 @@
 describe("Output poem as text", () => {
     it("Outputs current poem as text so user can edit", () => {
         cy.visit('http://localhost:3000/')
-        cy.get('[data-testid="cutting-text-area2"]').type("one and two and")
+        cy.get('[data-testid="cutting-text-area2"]').type("one and one and")
         cy.get('[data-testid="cut-btn"]').click()
-        cy.dragAndDrop(".text-snippet", '.wip-container')
+        cy.dragAndDrop(".text-snippet", '[data-testid="line-1"]')
             .wait(500)
-        cy.dragAndDrop(".text-snippet", '.wip-container')
+        cy.get('[data-testid="add-line-btn"]').click()
+        cy.dragAndDrop(".text-snippet", '[data-testid="line-2"]')
             .wait(500)
         cy.get('[data-testid="textify-btn"]').click()
-        cy.get('[data-testid="output-box"]').should("have.text", "two and one and \n")
+        cy.get('[data-testid="pop-up-text"]').should("have.text", "one and \none and \n")
     })
 })
