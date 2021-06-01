7d8a8b1149c96383d61bfb1ae462741b609d9c17
charlie-galb <galbraith.charlie@gmail.com>
2021-06-01 15:06:32 +0100

    Convert p elements to li

--- src/components/DragDropInstructions/DragDropInstructions.tsx
+++ src/components/DragDropInstructions/DragDropInstructions.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 export const DragDropInstructions = () => {
     return (
         <div className="drag-drop-instructions">
-            <p>Drag and drop the snippets into the blank lines provided,
-                creating whatever weird combinations you like. </p><br />
-            <p>When you're happy with what you've made, hit the textify 
-                button. Your poem will appear as text below.</p><br />
-            <p>And just like that, you're a poet. </p>
+            <ol start={3}>
+                <li>Drag and drop the snippets into the blank lines provided,
+                creating whatever weird combinations you like.</li>
+                <li>When you're happy with what you've made, hit the textify button. Your 
+                work will appear as formatted text.</li>
+                <li>And just like that, you're a poet.</li>
+            </ol>
         </div>
     )
 }
--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -3,13 +3,17 @@ import React from 'react'
 export const Intro = () => {
     return (
         <div className='intro-container'>
-            <p>Cut Up Creator is a tool for making collage poems 
-                in the style of William S. Burroughs.</p>
+            <p>Cut Up Creator is a tool for making collage poems in the style 
+                of William S. Burroughs. Just follow the steps below.</p>
             <br />
-            <p>Just copy text from three different sources (the more 
+            <ol>
+                <li>Copy text from three different sources (the more 
                 different the better) and paste them in the 
-                boxes below. Then hit the cut-up button to shred the text.</p>
-            <br />
+                boxes below.</li>
+                <li>
+                    Hit the cut-up button to shred the text.
+                </li>
+            </ol>
         </div>
     )
 }
