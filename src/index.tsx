c57d940d4e9795e2e8eae55ee15e8764f5efe556
charlie-galb <galbraith.charlie@gmail.com>
2021-06-01 15:06:41 +0100

    Update import from base to main

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 
 import App from './components/App/App'
 
-import './styles/base.scss';
+import './styles/main.scss';
 
 ReactDOM.render(<App/>, document.querySelector('#root'))
