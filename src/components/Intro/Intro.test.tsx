e756bc0c4be71ff515bc6072c6ea35974ef92722
charlie-galb <galbraith.charlie@gmail.com>
2021-06-03 15:23:16 +0100

    Remove separate text components

--- src/components/Intro/Intro.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from "@testing-library/react";
-
-import { Intro } from './Intro';
-
-test('renders without crashing', () => {
-    const intro = render(<Intro />);
-    expect(intro).toMatchSnapshot()
-});
