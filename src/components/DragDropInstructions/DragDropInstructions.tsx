66a0681bf96ad0a699bfac5001d1443b7ffd74d0
charlie-galb <galbraith.charlie@gmail.com>
2021-06-03 12:09:21 +0100

    Change li to p elements

--- src/components/DragDropInstructions/DragDropInstructions.tsx
+++ src/components/DragDropInstructions/DragDropInstructions.tsx
@@ -3,13 +3,10 @@ import React from 'react'
 export const DragDropInstructions = () => {
     return (
         <div className="drag-drop-instructions">
-            <ol start={3}>
-                <li>Drag and drop the snippets into the blank lines provided,
-                creating whatever weird combinations you like.</li>
-                <li>When you're happy with what you've made, hit the textify button. Your 
-                work will appear as formatted text.</li>
-                <li>And just like that, you're a poet.</li>
-            </ol>
+            <p>Drag and drop the snippets into the blank lines provided, creating 
+                whatever weird combinations you like. When you're happy with what 
+                you've made, just hit the textify button and your work will appear as 
+                formatted text.</p>
         </div>
     )
 }
