8853c4123e7bdfdbd653b636b08841ad7af225e9
charlie-galb <galbraith.charlie@gmail.com>
2021-06-03 15:26:53 +0100

    Add text const

---
+++ src/data/text.ts
@@ -0,0 +1,5 @@
+export const text = {
+    para1: "Cut Up Creator is a tool for making collage poems in the style of William S. Burroughs.",
+    para2: "To begin, just copy text from three different sources (the more different the better) and paste them in the boxes below. Then hit the cut-up button to shred the text.",
+    para3: "Drag and drop the snippets into the blank lines provided, creating whatever weird combinations you like. When you're happy with what you've made, hit the textify button. Your poem will appear as text below.",
+}
