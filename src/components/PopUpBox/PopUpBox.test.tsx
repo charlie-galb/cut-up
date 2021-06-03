e3d915faf72eecf8811c5bf9d011e9dd00818b3e
charlie-galb <galbraith.charlie@gmail.com>
2021-06-03 15:24:41 +0100

    Add test for render and callback

---
+++ src/components/PopUpBox/PopUpBox.test.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { render, fireEvent } from "@testing-library/react"
+
+import { mockContainers, mockLineOrder} from "../../utils/mockData"
+import { PopUpBox } from "./PopUpBox"
+
+const mockHandleClose = jest.fn()
+
+const sampleText= "Sample text"
+
+describe("PopUpBox", () => {
+    test("Renders text properly", () => {
+        const { getByTestId } = render(
+            <PopUpBox handleClose={mockHandleClose}>
+                {sampleText}
+            </PopUpBox>
+        )
+        expect(getByTestId('pop-up-text')).toHaveTextContent(sampleText)
+    })
+    test("Executes updates chunkContainers and lineOrder when clicked", () => {
+        const { getByTestId } = render(
+            <PopUpBox handleClose={mockHandleClose}>
+                {sampleText}
+            </PopUpBox>
+        )
+        fireEvent.click(getByTestId("close-icon"))
+        expect(mockHandleClose).toHaveBeenCalled()
+    })
+})
