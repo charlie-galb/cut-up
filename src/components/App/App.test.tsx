3fe510505737669e800adef2d20760ff3ce9750a
charlie-galb <galbraith.charlie@gmail.com>
2021-06-03 15:24:07 +0100

    Add tests for text content

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render } from "@testing-library/react";
+import { render, getByTestId } from "@testing-library/react";
 
 import { App } from './App';
 
-test('renders without crashing', () => {
-    const app = render(<App />);
-    expect(app).toMatchSnapshot();
-});
+import { text } from '../../data/text'
+
+const { para1, para2, para3 } = text
+
+describe('App', () => {
+    test('renders without crashing', () => {
+        const app = render(<App />);
+        expect(app.getByTestId('para-1')).toHaveTextContent(para1) 
+        expect(app.getByTestId('para-2')).toHaveTextContent(para2) 
+        expect(app.getByTestId('para-3')).toHaveTextContent(para3) 
+        expect(app).toMatchSnapshot();
+    });
+})
+    
