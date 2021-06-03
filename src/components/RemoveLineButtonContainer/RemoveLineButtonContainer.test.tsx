6fe6cee23f8e3e511a720cf088a44069adca21df
charlie-galb <galbraith.charlie@gmail.com>
2021-06-03 14:01:11 +0100

    Add render and callback tests

---
+++ src/components/RemoveLineButtonContainer/RemoveLineButtonContainer.test.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import { render, fireEvent } from "@testing-library/react"
+
+import { mockContainers, mockLineOrder} from "../../utils/mockData"
+import { RemoveLineButtonContainer } from "./RemoveLineButtonContainer"
+
+const mockUpdateChunkContainers = jest.fn()
+const mockUpdateLineOrder = jest.fn()
+
+describe("RemoveLineButtonContainer", () => {
+    test("Renders a button", () => {
+        const { getAllByRole } = render(
+            <RemoveLineButtonContainer 
+            chunkContainers={mockContainers}
+            lineOrder={mockLineOrder}
+            setChunkContainers={mockUpdateChunkContainers} 
+            setLineOrder={mockUpdateLineOrder}/>
+        )
+        expect(getAllByRole("button")).toHaveLength(1)
+    })
+    test("Executes updates chunkContainers and lineOrder when clicked", () => {
+        const { getByTestId } = render(
+            <RemoveLineButtonContainer 
+            chunkContainers={mockContainers}
+            lineOrder={mockLineOrder}
+            setChunkContainers={mockUpdateChunkContainers} 
+            setLineOrder={mockUpdateLineOrder}/>
+        )
+        fireEvent.click(getByTestId("add-line-btn"))
+        expect(mockUpdateChunkContainers).toHaveBeenCalled()
+        expect(mockUpdateLineOrder).toHaveBeenCalled()
+    })
+})
