d58b72b2120157d6cff19ed4bcf481995e6171e4
charlie-galb <galbraith.charlie@gmail.com>
2021-06-03 15:24:59 +0100

    Add test for button text

---
+++ src/components/TextSnippet/SortableTextSnippet.test.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { render, fireEvent } from "@testing-library/react"
+
+import { mockContainers, mockLineOrder, mockChunks} from "../../utils/mockData"
+import { SortableTextSnippet } from "./SortableTextSnippet"
+
+const mockSetPoemAsText = jest.fn()
+const mockDisplayPopUp = jest.fn()
+
+describe("SortableTextSnippet", () => {
+    test("Renders a button", () => {
+        const { getByRole } = render(
+            <SortableTextSnippet 
+                text={"test string"}
+                id={"one"} />
+        )
+        expect(getByRole("button")).toHaveTextContent("test string")
+    })
+})
