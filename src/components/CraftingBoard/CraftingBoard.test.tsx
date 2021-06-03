7d200e7ff6a7b6ad1958789c4f158db711b97437
charlie-galb <galbraith.charlie@gmail.com>
2021-06-03 15:41:15 +0100

    Add test for triggering PopUpBox

--- src/components/CraftingBoard/CraftingBoard.test.tsx
+++ src/components/CraftingBoard/CraftingBoard.test.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent, waitFor, screen, queryByText } from '@testing-library/react'
 
 import { CraftingBoard } from "./CraftingBoard"
 
 import { mockContainers, mockLineOrder, mockChunks } from "../../utils/mockData"
 
 describe("CraftingBoard", () => {
+    it('renders PopUpBox when the textify button is clicked', async () => {
+        const mockPoem = "Eeny, meeny, miney, mo"
+        const { getByText, getByTestId } = render(
+            <CraftingBoard
+                chunkContainers={mockContainers}
+                lineOrder={mockLineOrder}
+                wordChunks={mockChunks}
+                activeId={""}
+                poemAsText={mockPoem}
+                setChunkContainers={jest.fn()}
+                setLineOrder={jest.fn()}
+                setPoemAsText={jest.fn()}
+                />
+        )
+        const poem = screen.queryByText(mockPoem)
+        expect(poem).not.toBeInTheDocument()
+        fireEvent.click(getByTestId('textify-btn'))
+        await waitFor(() => {
+            expect(getByText(mockPoem)).toBeInTheDocument()
+          })
+    })
     it('renders correctly', () => {
         const board = render(
             <CraftingBoard
